796958c6aa8944b50981a2fc3cd630461f6ff16c
Sachin MM <109701268+Sachinsatya@users.noreply.github.com>
2024-04-15 21:54:39 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,8 +136,8 @@
     new Typed('.typed', {
       strings: typed_strings,
       loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
+      typeSpeed: 60,
+      backSpeed: 30,
       backDelay: 2000
     });
   }
